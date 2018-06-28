d67692e252154fbd9f79b874ae46be5c73723cae
alopezdomingo <analopezdomingo@hotmail.com>
2018-06-28 10:35:58 +0200

    Ejercicio Enlaces
    Falla el segundo enlace

--- pruebaJquery5.js
+++ pruebaJquery5.js
@@ -4,6 +4,7 @@ documento.ready(iniciar);
 
 function iniciar(){
 	$("#boton").click(verificar);
+	$("#boton2").click(seleccionarP);
 }
 function verificar()
 {
@@ -20,6 +21,29 @@ function verificar()
 		else
 		{
 			alert("no eres na");
+		}
+	}
+}
+function seleccionarP(){
+	if ($("#radioA").prop("checked"))
+	{
+		$("#demo").text("Has seleccionado: películas de acción");
+		$("#enlace2").attr("href", "https://www.esquire.com/es/actualidad/cine/g13104779/peliculas-de-terror-miedo-cine-mejores-cada-ano/");
+		$("#enlace2").attr("target","_blank");
+	}
+	else
+	{
+		if ($("#radioB").prop("checked"))
+		{
+			$("#demo").text("Has seleccionado: películas de terror");
+			$("#enlace2").attr("href", "http://decine21.com/listas-de-cine/lista/Las-100-mejores-peliculas-de-accion-93449");
+			$("#enlace2").attr("target","_blank");
+		}
+		else
+		{
+			$("#demo").text("Has seleccionado: películas de comedia");
+			$("#enlace2").attr("href", "https://www.elseptimoarte.net/peliculas/generos/comedia/");
+			$("#enlace2").attr("target","_blank");
 		}
 	}
 }
